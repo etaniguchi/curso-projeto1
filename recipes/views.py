908eb76919719baa834a744ea04168b809ddd976
Eduardo Taniguchi <etaniguchi@gmail.com>
2024-01-18 09:08:18 -0300

    aula 49

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,7 @@ def home(request):
 
 def recipe(request,id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe()
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
