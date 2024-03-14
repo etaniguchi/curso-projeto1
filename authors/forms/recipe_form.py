24576338afab55fd39af3f873338f5fe3e01b22c
Eduardo Taniguchi <etaniguchi@gmail.com>
2024-03-14 19:55:30 -0300

    formulario da receita

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
 from django import forms
 from recipes.models import Recipe
+from django.core.exceptions import ValidationError
 from utils.django_forms import add_attr
 
 class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
 
         add_attr(self.fields.get('title'), 'class', 'span-2')
         add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
@@ -24,12 +28,42 @@ class AuthorRecipeForm(forms.ModelForm):
                     ('Porções', 'Porções'),
                     ('Pedaços', 'Pedaços'),
                     ('Pessoas', 'Pessoas'),
-                )
+                ),
             ),
             'preparation_time_unit': forms.Select(
                 choices=(
                     ('Horas', 'Horas'),
                     ('Minutos', 'Minutos'),
-                )
+                ),
             ),
         }
+
+
+    def clean(self, *args, **kwargs):
+        """
+        Faz o tratamento da quantidade minima digitado no formulario para os campos:
+        - title
+        - description
+        """
+
+        super_clean = super().clean(*args, **kwargs)
+        cleaned_data = self.cleaned_data
+        
+        title = cleaned_data.get('title')
+        description = cleaned_data.get('description')
+
+        if len(title) < 5:
+            self._my_errors['title'].append(
+                'Title must have at least 5 characters.'
+                )
+            
+        if title == description:
+            self._my_errors['title'].append('Cannot be equal to description')
+            self._my_errors['description'].append('Cannot be equal to title')
+            
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+
+        return super_clean
+    
+
--- authors/views.py
+++ authors/views.py
@@ -114,8 +114,21 @@ def dashboard_recipe_edit(request, id):
     
     form = AuthorRecipeForm(
         data = request.POST or None,
+        files = request.FILES or None, # no form.html adicionar enctype="multipart/form-data"
         instance = recipe,
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Sua receita foi salva com sucesso!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(
         request,
@@ -124,3 +137,42 @@ def dashboard_recipe_edit(request, id):
             'form': form,
         },
     )
+
+@login_required(login_url='authors:login',redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data = request.POST or None,
+        files = request.FILES or None, # no form.html adicionar enctype="multipart/form-data"
+    )
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'form': form,
+            'form_action':reverse('authors:dashboard_recipe_create'),
+        },
+    )
+
+@login_required(login_url='authors:login',redirect_field_name='next')
+def dashboard_recipe_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    form = AuthorRecipeForm(
+        data = request.POST or None,
+        files = request.FILES or None, # no form.html adicionar enctype="multipart/form-data"
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Sua receita foi criada com sucesso!')
+
+    return redirect(reverse('authors:dashboard'))
