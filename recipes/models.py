2ade258583c034b0045065c63fc473c327e713f1
Eduardo Taniguchi <etaniguchi@gmail.com>
2024-03-15 06:06:15 -0300

    form do delete

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -34,3 +35,10 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
